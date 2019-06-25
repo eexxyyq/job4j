bee13fa1172d19e42634750667c0e1a573b87bfa
Yaroslav Tikhonov <eexxyyq@yandex.ru>
2019-06-25 18:35:45 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне[#151553]

---
+++ chapter_001/src/main/java/ru/job4j/loop/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Изучение циклов
+ * @author Tikhonov Yaroslav (eexxyyq@yandex.ru)
+ */
+package ru.job4j.loop;
---
+++ chapter_001/src/test/java/ru/job4j/loop/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Изучение циклов TEST
+ * @author Tikhonov Yaroslav (eexxyyq@yandex.ru)
+ */
+package ru.job4j.loop;
