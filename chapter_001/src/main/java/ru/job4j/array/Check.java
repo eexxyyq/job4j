c32029b5e0df6111ca339fe791510e765e877fd7
Yaroslav Tikhonov <eexxyyq@yandex.ru>
2019-06-27 11:58:14 +0300

    6.3. Массив заполнен true или false[#151563]

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,40 @@
+package ru.job4j.array;
+
+/**
+ * Проверка массива на однородность элементов
+ */
+public class Check {
+    /**
+     * Решение через временную переменную для сравнения с остальными
+     * @param array массив
+     * @return true/false
+     */
+    public boolean check(boolean[] array) {
+        boolean chk = false;
+        boolean tmp = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] == tmp) {
+                chk = true;
+            } else {
+                chk = false;
+                break;
+            }
+        }
+        return chk;
+    }
+    /**
+     * Решение через сравнение соседних элементов
+     * @param array массив
+     * @return true/false
+     */
+    public boolean mono(boolean[] array) {
+        boolean result = true;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] != array[i + 1]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/CheckTest.java
@@ -0,0 +1,72 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CheckTest {
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, false, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenDataNotMonoByFalseThenFalse() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {false, false, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenDataMonoByFalseThenTrue() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {false, false, false};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataForCheckIsMonoByTrueThenTrue() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, true};
+        boolean result = check.check(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataForCheckIsNotMonoByTrueThenFalse() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, false, true};
+        boolean result = check.check(input);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenDataForCheckIsNotMonoByFalseThenFalse() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {false, false, true};
+        boolean result = check.check(input);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenDataForCheckIsMonoByFalseThenTrue() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {false, false, false};
+        boolean result = check.check(input);
+        assertThat(result, is(true));
+    }
+
+}
