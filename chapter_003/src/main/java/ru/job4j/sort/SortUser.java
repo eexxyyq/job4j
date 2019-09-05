eff82339aca8341d7a5f4a3d8f22ecc20b9f6085
Yaroslav TIkhonov <eexxyyq@yandex.ru>
2019-09-05 19:26:20 +0300

    some fix

--- chapter_003/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sort/SortUser.java
@@ -24,7 +24,8 @@ public class SortUser {
         list.sort(new Comparator<User>() {
             @Override
             public int compare(User o1, User o2) {
-                return o1.getName().compareTo(o2.getName()) == 0 ? Integer.compare(o1.getAge(), o2.getAge()) : o1.getName().compareTo(o2.getName());
+                return o1.getName().compareTo(o2.getName()) == 0
+                        ? Integer.compare(o1.getAge(), o2.getAge()) : o1.getName().compareTo(o2.getName());
             }
         });
         return list;
