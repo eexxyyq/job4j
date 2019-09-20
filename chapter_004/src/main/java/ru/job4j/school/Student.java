dcfd92b8480cc6b4c39a10c67f0c0b1560fc589d
Yaroslav Tikhonov <eexxyyq@gmail.com>
2019-09-20 20:58:50 +0500

    1. Фильтрация учеников.[#151505]

---
+++ chapter_004/src/main/java/ru/job4j/school/Student.java
@@ -0,0 +1,27 @@
+package ru.job4j.school;
+
+public class Student {
+    private int score;
+    private String name;
+
+    public Student(int score, String name) {
+        this.score = score;
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/school/SchoolTest.java
@@ -0,0 +1,55 @@
+package ru.job4j.school;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class SchoolTest {
+    private List<Student> getList() {
+        List<Student> result = new ArrayList<>();
+        for (int i = 10; i <= 100; i += 10) {
+            result.add(new Student(i, "student" + i));
+        }
+        return result;
+    }
+    @Test
+    public void whenGetListForAClass() {
+        School school = new School();
+        List<Student> listOfAllStudents = getList();
+        Predicate<Student> predicate = student -> student.getScore() >= 70 && student.getScore() <= 100;
+        List<Student> expect = Arrays.asList(listOfAllStudents.get(6),
+                listOfAllStudents.get(7),
+                listOfAllStudents.get(8),
+                listOfAllStudents.get(9));
+        List<Student> result = school.collect(listOfAllStudents, predicate);
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenGetListForBClass() {
+        School school = new School();
+        List<Student> listOfAllStudents = getList();
+        Predicate<Student> predicate = student -> student.getScore() >= 50 && student.getScore() < 70;
+        List<Student> expect = Arrays.asList(listOfAllStudents.get(4),
+                listOfAllStudents.get(5));
+        List<Student> result = school.collect(listOfAllStudents, predicate);
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenGetListForCClass() {
+        School school = new School();
+        List<Student> listOfAllStudents = getList();
+        Predicate<Student> predicate = student -> student.getScore() > 0 && student.getScore() < 50;
+        List<Student> expect = Arrays.asList(listOfAllStudents.get(0),
+                listOfAllStudents.get(1),
+                listOfAllStudents.get(2),
+                listOfAllStudents.get(3));
+        List<Student> result = school.collect(listOfAllStudents, predicate);
+        assertThat(result, is(expect));
+    }
+}
