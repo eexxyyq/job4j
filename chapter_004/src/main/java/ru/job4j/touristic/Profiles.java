58aab0c5c24c8801ebac9e5f69849e0b6c9d5e55
Yaroslav Tikhonov <eexxyyq@gmail.com>
2019-11-04 17:20:45 +0500

    3. Уникальность элементов и сортировка.[#151507]

--- chapter_004/src/main/java/ru/job4j/touristic/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/touristic/Profiles.java
@@ -1,5 +1,6 @@
 package ru.job4j.touristic;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,6 +8,6 @@ class Profiles {
     static List<Address> collect(List<Profile> profiles) {
         return profiles.stream().map(
                 Profile::getAddress
-        ).collect(Collectors.toList());
+        ).sorted(Comparator.comparing(Address::getCity)).distinct().collect(Collectors.toList());
     }
 }
--- chapter_004/src/test/java/ru/job4j/touristic/ProfilesTest.java
+++ chapter_004/src/test/java/ru/job4j/touristic/ProfilesTest.java
@@ -15,17 +15,35 @@ public class ProfilesTest {
         }
         return profiles;
     }
-    private List<Address> expected() {
+    private List<Profile> list2() {
+        List<Profile> profiles = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            profiles.add(new Profile(new Address("city_0", "street_0", 0, 0)));
+        }
+        return profiles;
+    }
+    private List<Address> expected1() {
         List<Address> res = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             res.add(new Address("city_" + i, "street_" + i, i, i));
         }
         return res;
     }
+    private List<Address> expected2() {
+        List<Address> res = new ArrayList<>();
+        res.add(new Address("city_0", "street_0", 0, 0));
+        return res;
+    }
     @Test
     public void whenWantToSomeAddress() {
         List<Profile> profiles = list();
         List<Address> res = Profiles.collect(profiles);
-        assertThat(res, is(expected()));
+        assertThat(res, is(expected1()));
+    }
+    @Test
+    public void whenWantToSomeUniqAddress() {
+        List<Profile> profiles = list2();
+        List<Address> res = Profiles.collect(profiles);
+        assertThat(res, is(expected2()));
     }
 }
