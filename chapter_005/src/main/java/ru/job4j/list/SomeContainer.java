b57f8d03faf1c4e1ddc65146e117eec2ab094cd2
Yaroslav Tikhonov <eexxyyq@yandex.ru>
2019-11-26 20:52:12 +0300

    5.3.1. Создать динамический список на базе массива.[#198042]

---
+++ chapter_005/src/main/java/ru/job4j/list/SomeContainer.java
@@ -0,0 +1,59 @@
+package ru.job4j.list;
+
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SomeContainer<E> implements Iterable{
+    private Object[] container;
+    private int index = 0;
+    private int countMod = 0;
+    SomeContainer() {
+        this.container = new Object[10];
+    }
+
+    private void getNewSize() {
+        if (index == this.container.length) {
+            this.container = Arrays.copyOf(this.container, this.container.length * 2);
+            countMod++;
+        }
+    }
+
+    public void add(E model) {
+        getNewSize();
+        this.container[index++] = model;
+        countMod++;
+    }
+
+    public E get(int index) {
+        E result = null;
+        if (this.container[index] != null) {
+            result = (E) this.container[index];
+        }
+        return result;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new Iterator() {
+            int indexIter = 0;
+            int checkMod = countMod;
+            @Override
+            public boolean hasNext() {
+                return indexIter < container.length;
+            }
+
+            @Override
+            public Object next() {
+                if (checkMod != countMod) {
+                    throw new ConcurrentModificationException();
+                }
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return get(indexIter++);
+            }
+        };
+    }
+}
