978cf574933cf764f504bc8e59e24629b4232f92
Yaroslav Tikhonov <eexxyyq@yandex.ru>
2019-11-28 21:18:58 +0300

    1. Реализовать коллекцию Set на массиве[#198068] fix alg

--- chapter_005/src/main/java/ru/job4j/set/SimpleSet.java
+++ chapter_005/src/main/java/ru/job4j/set/SimpleSet.java
@@ -2,17 +2,48 @@ package ru.job4j.set;
 
 import ru.job4j.list.SomeContainer;
 
-public class SimpleSet<T> extends SomeContainer<T> {
-    @Override
-    public void add(T model) {
-        int count = 0;
-        for (int i = 0; i < super.getSize(); i++) {
-            if (model == super.get(i) ) {
-                count++;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleSet<T> implements Iterable<T> {
+    private SomeContainer<T> set = new SomeContainer<>();
+    private int index = 0;
+    private boolean checkRepeat(T model) {
+        boolean result = false;
+        for (int i = 0; i < set.getSize(); i++) {
+            if (model.equals(set.get(i))) {
+                result = true;
+                break;
             }
         }
-        if (count == 0) {
-            super.add(model);
+        return result;
+    }
+
+    public int getSize() {
+        return set.getSize();
+    }
+
+    public void add(T model) {
+        if (!checkRepeat(model)) {
+            set.add(model);
         }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                return index < getSize();
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return set.get(index++);
+            }
+        };
     }
 }
