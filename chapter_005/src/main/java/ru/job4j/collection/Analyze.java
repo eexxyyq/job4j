98dd845513346b864a9cf654ce0f99d07b53669e
Yaroslav TIkhonov <eexxyyq@yandex.ru>
2019-12-07 18:35:27 +0300

    Статистику по коллекции.[#198036]

---
+++ chapter_005/src/main/java/ru/job4j/collection/Analyze.java
@@ -0,0 +1,102 @@
+package ru.job4j.collection;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Analyze {
+    public Info diff(List<User> previous, List<User> current) {
+        int addedNewUsers = 0;
+        int changedUsers = 0;
+        int deletedUser = 0;
+        int countRepeat = 0;
+        for (User user : previous) {
+            for (User user1 : current) {
+                if (user.getId() == user1.getId()) {
+                    countRepeat++;
+                }
+                if (user.getId() == user1.getId() && !user.getName().equals(user1.getName())) {
+                    changedUsers++;
+                }
+            }
+        }
+        if (current.size() - countRepeat > 0) {
+            addedNewUsers = current.size() - countRepeat;
+        }
+        if (previous.size() - countRepeat > 0) {
+            deletedUser = previous.size() - countRepeat;
+        }
+        return new Info(addedNewUsers, changedUsers, deletedUser);
+    }
+
+    public static class User {
+        private int id;
+        private String name;
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "id = " + id
+                    + "\n" + "name = " + name;
+        }
+    }
+
+    public static class Info {
+        int addedNewUsers;
+        int changedUsers;
+        int deletedUser;
+
+        public Info(int addedNewUsers, int changedUsers, int deletedUser) {
+            this.addedNewUsers = addedNewUsers;
+            this.changedUsers = changedUsers;
+            this.deletedUser = deletedUser;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Info info = (Info) o;
+            return addedNewUsers == info.addedNewUsers &&
+                    changedUsers == info.changedUsers &&
+                    deletedUser == info.deletedUser;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(addedNewUsers, changedUsers, deletedUser);
+        }
+
+        @Override
+        public String toString() {
+            return  "addedNewUsers = " + addedNewUsers
+                    + "\n"
+                    + "changedUsers = " + changedUsers
+                    + "\n"
+                    + "deletedUser = " + deletedUser;
+        }
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/collection/AnalyzeTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class AnalyzeTest {
+
+    @Test
+    public void whenAnalyzeTwoCollections() {
+        Analyze analyze = new Analyze();
+        Analyze.User user1 = new Analyze.User(1, "asd");
+        Analyze.User user2 = new Analyze.User(2, "bsd");
+        Analyze.User user3 = new Analyze.User(3, "qwe");
+        Analyze.User user4 = new Analyze.User(1, "jhg");
+        Analyze.User user5 = new Analyze.User(5, "frt");
+        Analyze.User user6 = new Analyze.User(6, "yhb");
+        List<Analyze.User> previous = new ArrayList<>(List.of(user1, user2, user3));
+        List<Analyze.User> current = new ArrayList<>(List.of(user4, user2, user5, user6));
+        Analyze.Info expected = new Analyze.Info(2, 1, 1);
+        Analyze.Info result = analyze.diff(previous, current);
+        assertThat(result, is(expected));
+
+
+    }
+}
