71237d9f454142d9f9a42f78a318c8f05b5b9a13
Yaroslav TIkhonov <eexxyyq@yandex.ru>
2020-01-03 21:04:01 +0300

    postTest fix

--- chapter_005/src/test/java/ru/job4j/post/PostTest.java
+++ chapter_005/src/test/java/ru/job4j/post/PostTest.java
@@ -10,21 +10,25 @@ public class PostTest {
     @Test
     public void postTest() {
         Post post = new Post();
-        User user1 = new User("user1", "email1");
-        User user2 = new User("user2", "email2");
-        User user3 = new User("user3", "email3");
-        User user4 = new User("user4", "email4");
-        user4.addNewEmail("email5");
-        user4.addNewEmail("email1");
-        user3.addNewEmail("email4");
+        User user1 = new User("user1", "xxx@ya.ru");
+        User user2 = new User("user2", "foo@gmail.com");
+        User user3 = new User("user3", "xyz@pisem.net");
+        User user4 = new User("user4", "ups@pisem.net");
+        User user5 = new User("user5", "xyz@pisem.net");
+        user1.addNewEmail("foo@gmail.com");
+        user1.addNewEmail("lol@mail.ru");
+        user2.addNewEmail("ups@pisem.net");
+        user3.addNewEmail("vasya@pupkin.com");
+        user4.addNewEmail("aaa@bbb.ru");
         post.addNewUser(user1);
         post.addNewUser(user2);
         post.addNewUser(user3);
         post.addNewUser(user4);
+        post.addNewUser(user5);
         post.rebase();
         Set<User> expected = new HashSet<>();
-        expected.add(user3);
         expected.add(user2);
+        expected.add(user3);
         Set<User> result = post.getBase();
         assertThat(result, is(expected));
     }
