d7884d56f72129ece3b20090918ed2182de1c83f
eexxyyq <eexxyyq@gmail.com>
2020-02-16 18:18:03 +0300

    fix pom.xml dependencies

--- chapter_006/src/test/java/ru/job4j/io/SearchTest.java
+++ chapter_006/src/test/java/ru/job4j/io/SearchTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SearchTest {
@@ -14,12 +15,14 @@ public class SearchTest {
         List<String> list = new ArrayList<>();
         list.add("xml");
         list.add("txt");
-        List<File> resultList = search.files("../../../TempDir/", list);
+        List<File> resultList = search.files("TempDir/", list);
         List<String> result = new ArrayList<>();
         for (File f : resultList) {
             result.add(f.getName());
         }
-        List<String> expect = new ArrayList<>(List.of("test.xml", "test.txt", "test4.txt"));
+        List<String> expect = new ArrayList<>(List.of("test2.xml", "test1.txt", "test4.txt"));
+        Collections.sort(result);
+        Collections.sort(expect);
         assertThat(result, is(expect));
     }
 }
